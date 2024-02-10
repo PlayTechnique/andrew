f6436ae4f8d3bbb3892939be671b8b1887f70c69
Gwyn <gwyn@playtechnique.io>
2024-02-09 21:04:58 -0700

    Serve an index.html file if it exists.

--- server.go
+++ server.go
@@ -1,10 +1,36 @@
 package andrew
 
 import (
-	"fmt"
 	"net/http"
+	"os"
 )
 
-func ServeRoot(w http.ResponseWriter, _ *http.Request) {
-	fmt.Fprintf(w, "ABC")
+type Server struct {
+	ContentRoot string
+	HttpServer  http.Handler
+}
+
+// serveUp will check to see if a static file exists  that matches the name of the request.
+// If that file does exist, it gets served.
+// In the special case that you are serving from a directory that contains other directories,
+// and those child directories contain files but the cwd does not, an index.html will be
+// automatically generated.
+func (s *Server) ServeUp(w http.ResponseWriter, r *http.Request) {
+	path := r.URL.Path
+	trailingCharacter := path[len(path)-1]
+
+	if trailingCharacter == '/' {
+		indexPath := s.ContentRoot + path + "index.html"
+
+		if _, err := os.Stat(indexPath); os.IsNotExist(err) {
+			s.generateIndex(w, r)
+			return
+		}
+	}
+
+	s.HttpServer.ServeHTTP(w, r)
+}
+
+func (s *Server) generateIndex(w http.ResponseWriter, r *http.Request) {
+	return
 }
--- server_test.go
+++ server_test.go
@@ -3,31 +3,83 @@ package andrew_test
 import (
 	"github.com/playtechnique/andrew"
 	"io"
+	"net/http"
 	"net/http/httptest"
+	"os"
+	"slices"
 	"testing"
 )
 
-func TestWhenADirectoryContainsNoFilesItsParentGetsNothing(t *testing.T) {
+// TestServeIndexHtmlIfItExists will check an incoming request. If it is for a directory,
+// and that directory contains an index.html, that index.html will be served.
+func TestServeIndexHtmlIfItExists(t *testing.T) {
 	rec := httptest.NewRecorder()
 	req := httptest.NewRequest("GET", "/", nil)
-	andrew.ServeRoot(rec, req)
+
+	contentRoot := "testdata/onlyIndexHtml"
+	expected, err := os.ReadFile(contentRoot + "/index.html")
+
+	if err != nil {
+		t.Fatalf("Could not find test data: %v", err)
+	}
+
+	server := andrew.Server{ContentRoot: contentRoot, HttpServer: http.FileServer(http.Dir(contentRoot))}
+	server.ServeUp(rec, req)
+
 	res := rec.Result()
 	defer res.Body.Close()
 
-	data, err := io.ReadAll(res.Body)
+	indexContents, err := io.ReadAll(res.Body)
 	if err != nil {
 		t.Errorf("expected error to be nil got %v", err)
 	}
-	if string(data) != "ABC" {
-		t.Errorf("expected ABC got %v", string(data))
+
+	actual := indexContents
+
+	if !slices.Equal(expected, actual) {
+		t.Errorf("\nexpected:\n %s \nactual:\n %s", string(expected), string(actual))
 	}
-
 }
 
-func TestWhenADirectoryContainsOneFileItsParentGetsAnIndexHtmlContainingIt(t *testing.T) {
+// TestServeIndexHtmlIfItExists will check an incoming request for a corresponding file
+// on the file system and serve it.
+// func TestServeHtmlIfItExists(t *testing.T) {
+// 	rec := httptest.NewRecorder()
+// 	req := httptest.NewRequest("GET", "/", nil)
+// 	andrew.ServeUp(rec, req)
+// 	res := rec.Result()
+// 	defer res.Body.Close()
+//
+// 	data, err := io.ReadAll(res.Body)
+// 	if err != nil {
+// 		t.Errorf("expected error to be nil got %v", err)
+// 	}
+// 	if string(data) != "ABC" {
+// 		t.Errorf("expected ABC got %v", string(data))
+// 	}
+// }
 
-}
-
-func TestWhenADirectoryContainsManyFilesItsParentGetsAnIndexHtmlContainingThem(t *testing.T) {
-
-}
+// func TestWhenADirectoryContainsNoFilesItsParentGetsNothing(t *testing.T) {
+// 	rec := httptest.NewRecorder()
+// 	req := httptest.NewRequest("GET", "/", nil)
+// 	andrew.ServeRoot(rec, req)
+// 	res := rec.Result()
+// 	defer res.Body.Close()
+//
+// 	data, err := io.ReadAll(res.Body)
+// 	if err != nil {
+// 		t.Errorf("expected error to be nil got %v", err)
+// 	}
+// 	if string(data) != "ABC" {
+// 		t.Errorf("expected ABC got %v", string(data))
+// 	}
+//
+// }
+//
+// func TestWhenADirectoryContainsOneFileItsParentGetsAnIndexHtmlContainingIt(t *testing.T) {
+//
+// }
+//
+// func TestWhenADirectoryContainsManyFilesItsParentGetsAnIndexHtmlContainingThem(t *testing.T) {
+//
+// }
